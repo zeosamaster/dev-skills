ac824655c38148e09062f18ecc000a95edb8145f
Fábio Matias <fabio_mcm2@hotmail.com>
2021-11-25 21:07:29 +0000

    fix: use REACT_APP_ prefix for env vars

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  process.env.SUPABASE_URL ||
-  process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://pklxqwkcbkmdssojczdq.supabase.co";
+  process.env.REACT_APP_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey =
-  process.env.SUPABASE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  process.env.REACT_APP_SUPABASE_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
