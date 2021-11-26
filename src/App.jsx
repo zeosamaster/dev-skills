1a9a588bdfbd7acf0aa85fe7362960ec2d510bbc
Fábio Matias <fabio_mcm2@hotmail.com>
2021-11-26 22:51:31 +0000

    feat: improve page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { ChakraProvider, SimpleGrid, Heading } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/layout";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import { Devs } from "./pages/Devs";
 import { AddDev } from "./pages/AddDev";
+import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Login } from "./pages/Login";
 import { LoginMagicLink } from "./pages/LoginMagicLink";
@@ -13,21 +15,25 @@ import { Signup } from "./pages/Signup";
 function App() {
   return (
     <ChakraProvider>
-      <main>
-        <SimpleGrid columns={1} spacing={10}>
-          <Heading>Dev DAO - Dev Skills</Heading>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Devs />} />
-              <Route path="/add" element={<AddDev />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/login-magic-link" element={<LoginMagicLink />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-          </BrowserRouter>
-          <Footer />
-        </SimpleGrid>
-      </main>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Flex flexFlow="column" h="full">
+            <Box flex="2 0 auto">
+              <Routes>
+                <Route path="/" element={<Devs />} />
+                <Route path="/add" element={<AddDev />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/login-magic-link" element={<LoginMagicLink />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </Box>
+            <Box flex="0">
+              <Footer />
+            </Box>
+          </Flex>
+        </main>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Flex, Heading } from "@chakra-ui/layout";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={6}
+      bg="teal.500"
+      color="white"
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+          <Link to="/">Dev DAO - Dev Skills</Link>
+        </Heading>
+      </Flex>
+    </Flex>
+  )
+}
